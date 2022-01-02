e1c46a08c809a178432d8df455994a32e2d87deb
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2022-01-02 11:20:49 -0300

    move production code to their own files

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,4 +1,5 @@
-import { LoadFacebookUserApi } from '@/data/contracts/apis'
+import { FacebookApi } from '@/infra/apis'
+import { HttpGetClient } from '@/infra/http'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 
@@ -31,35 +32,3 @@ describe('FacebookApi', () => {
     })
   })
 })
-
-export class FacebookApi {
-  private readonly baseUrl: string = 'https://graph.facebook.com'
-
-  constructor (
-    private readonly httpClient: HttpGetClient,
-    private readonly clientId: string,
-    private readonly clientSecret: string
-  ) {}
-
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
-    await this.httpClient.get({
-      url: `${this.baseUrl}/oauth/access_token`,
-      params: {
-        client_id: this.clientId,
-        client_secret: this.clientSecret,
-        grant_type: 'client_credentials'
-      }
-    })
-  }
-}
-
-export interface HttpGetClient {
-  get: (params: HttpGetClient.Params) => Promise<void>
-}
-
-export namespace HttpGetClient {
-  export type Params = {
-    url: string
-    params: object
-  }
-}
