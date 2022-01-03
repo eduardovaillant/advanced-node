ed972c10396234a2914f78ff1209b8dbb54eb863
Eduardo Brito Vaillant <eduardo.brito@linkapi.com.br>
2022-01-03 15:44:37 -0300

    isolate sut creation

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -5,24 +5,30 @@ import axios from 'axios'
 jest.mock('axios')
 
 describe('AxiosHttpClient', () => {
+  let fakeAxios: jest.Mocked<typeof axios>
+  let sut: AxiosHttpClient
+  let url: string
+  let params: object
+
+  beforeAll(() => {
+    url = 'any_url'
+    params = { any: 'any' }
+    fakeAxios = axios as jest.Mocked<typeof axios>
+  })
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient()
+  })
+
   it('should call get with correct params', async () => {
-    const fakeAxios = axios as jest.Mocked<typeof axios>
-    const sut = new AxiosClient()
-    const params = {
-      url: 'any_url',
-      params: {
-        any: 'any'
-      }
-    }
+    await sut.get({ url, params })
 
-    await sut.get(params)
-
-    expect(fakeAxios.get).toHaveBeenCalledWith('any_url', { params: { any: 'any' } })
+    expect(fakeAxios.get).toHaveBeenCalledWith(url, { params: params })
     expect(fakeAxios.get).toHaveBeenCalledTimes(1)
   })
 })
 
-export class AxiosClient {
+export class AxiosHttpClient {
   async get (args: HttpGetClient.Params): Promise<void> {
     await axios.get(args.url, { params: args.params })
   }
