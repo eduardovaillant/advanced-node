3bb556d80dbd9d23a180c73be10f0c21eb0c7286
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2022-01-06 17:18:38 -0300

    ensure FacebookLoginController returns 400 if token is empty

---
+++ tests/application/controllores/facebook-login.spec.ts
@@ -0,0 +1,26 @@
+describe('FacebookLoginControler', () => {
+  it('should return 400 if token is empty', async () => {
+    const sut = new FacebookLoginControler()
+
+    const httpResponse = await sut.handle({ token: '' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The token field is required!')
+    })
+  })
+})
+
+class FacebookLoginControler {
+  async handle (httpRequest: any): Promise<HttpResponse> {
+    return {
+      statusCode: 400,
+      data: new Error('The token field is required!')
+    }
+  }
+}
+
+type HttpResponse = {
+  statusCode: number
+  data: any
+}
