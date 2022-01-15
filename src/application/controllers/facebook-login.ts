5ea67a9ad822cc2b1369a8a7a001f0087add9d63
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2022-01-15 17:55:30 -0300

    convert FacebookAuthenticationUseCase into a function

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,13 +1,10 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse, ok, unauthorized } from '@/application/helpers'
 import { Validator, ValidationBuilder } from '@/application/validation'
-import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/entities'
+import { FacebookAuthentication } from '@/domain/use-cases'
 
-type HttpRequest = {
-  token: string
-}
-
+type HttpRequest = { token: string}
 type Model = Error | {accessToken: string}
 
 export class FacebookLoginControler extends Controller {
@@ -18,7 +15,7 @@ export class FacebookLoginControler extends Controller {
   }
 
   async perform ({ token }: HttpRequest): Promise<HttpResponse<Model>> {
-    const accessToken = await this.facebookAuthentication.perform({ token })
+    const accessToken = await this.facebookAuthentication({ token })
     return accessToken instanceof AccessToken
       ? ok({ accessToken: accessToken.value })
       : unauthorized()
