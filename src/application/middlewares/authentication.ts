e21869c25674e49a21b7067a838929c3d7aa8b45
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2022-01-19 07:19:16 -0300

    move AuthenticationMiddleware to its own file

---
+++ src/application/middlewares/authentication.ts
@@ -0,0 +1,27 @@
+import { RequiredStringValidator } from '@/application/validation'
+import { HttpResponse, ok, unauthorized } from '@/application/helpers'
+import { Authorize } from '@/domain/use-cases'
+
+type HttpRequest = { authorization: string }
+type Model = Error | { userId: string }
+
+export class AuthenticationMiddleware {
+  constructor (
+    private readonly authorize: Authorize
+  ) {}
+
+  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
+    if (!this.validate({ authorization })) return unauthorized()
+    try {
+      const userId = await this.authorize({ token: authorization })
+      return ok({ userId })
+    } catch {
+      return unauthorized()
+    }
+  }
+
+  private validate ({ authorization }: HttpRequest): boolean {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+    return error === undefined
+  }
+}
--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,7 +1,5 @@
+import { AuthenticationMiddleware } from '@/application/middlewares'
 import { UnauthorizedError } from '@/application/errors'
-import { HttpResponse, ok, unauthorized } from '@/application/helpers'
-import { RequiredStringValidator } from '@/application/validation'
-import { Authorize } from '@/domain/use-cases'
 
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware
@@ -71,27 +69,3 @@ describe('AuthenticationMiddleware', () => {
     })
   })
 })
-
-type HttpRequest = { authorization: string }
-type Model = Error | { userId: string }
-
-export class AuthenticationMiddleware {
-  constructor (
-    private readonly authorize: Authorize
-  ) {}
-
-  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
-    if (!this.validate({ authorization })) return unauthorized()
-    try {
-      const userId = await this.authorize({ token: authorization })
-      return ok({ userId })
-    } catch {
-      return unauthorized()
-    }
-  }
-
-  private validate ({ authorization }: HttpRequest): boolean {
-    const error = new RequiredStringValidator(authorization, 'authorization').validate()
-    return error === undefined
-  }
-}
