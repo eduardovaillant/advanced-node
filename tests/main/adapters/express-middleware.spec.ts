bab96c5b3ef941c1815a222817221fb8b47a4a1e
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2022-01-20 08:31:35 -0300

    ensure ExpressMiddleware responds with correct error and statusCode

--- tests/main/adapters/express-middleware.spec.ts
+++ tests/main/adapters/express-middleware.spec.ts
@@ -16,6 +16,10 @@ describe('ExpressMiddleware', () => {
     res = getMockRes().res
     next = getMockRes().next
     middleware = mock()
+    middleware.handle.mockResolvedValue({
+      statusCode: 500,
+      data: { error: 'any_error' }
+    })
   })
 
   beforeEach(() => {
@@ -37,12 +41,22 @@ describe('ExpressMiddleware', () => {
     expect(middleware.handle).toHaveBeenCalledWith({ })
     expect(middleware.handle).toHaveBeenCalledTimes(1)
   })
+
+  it('should responde with correct error and statusCode', async () => {
+    await sut(req, res, next)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ error: 'any_error' })
+    expect(res.json).toHaveBeenCalledTimes(1)
+  })
 })
 
 type Adapter = (middleware: Middleware) => RequestHandler
 
 const adaptExpressMiddleware: Adapter = middleware => async (req, res, next) => {
-  await middleware.handle({ ...req.headers })
+  const { statusCode, data } = await middleware.handle({ ...req.headers })
+  res.status(statusCode).json(data)
 }
 
 export interface Middleware {
