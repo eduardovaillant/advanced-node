7ce66ebed33e374396785830a1ea49b6a62ec89e
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2022-01-28 18:43:42 -0300

    ensure ChangeProfilePicture calls LoadUserProfile with correct input

--- src/domain/contracts/repos/user-profile.ts
+++ src/domain/contracts/repos/user-profile.ts
@@ -1,7 +1,16 @@
 export interface SaveUserPicture {
-  savePicture: (params: SaveUserPicture.Input) => Promise<void>
+  savePicture: (input: SaveUserPicture.Input) => Promise<void>
 }
 
 export namespace SaveUserPicture {
   export type Input = { pictureUrl?: string }
+}
+
+export interface LoadUserProfile {
+  load: (input: LoadUserProfile.Input) => Promise<void>
+}
+
+export namespace LoadUserProfile {
+  export type Input = { id: string }
+
 }
