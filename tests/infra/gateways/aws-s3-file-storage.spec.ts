0dafa5ed5b799742e35c44a85f159b967ab20abc
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2022-02-12 08:55:31 -0300

    ensure AwsS3FileStorage calls putObject with correct input

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -1,33 +1,72 @@
-import { config } from 'aws-sdk'
+import { UploadFile } from '@/domain/contracts/gateways'
+import { config, S3 } from 'aws-sdk'
+import { String } from 'aws-sdk/clients/appstream'
+import { mocked } from 'ts-jest/utils'
 
 jest.mock('aws-sdk')
 
 describe('AwsS3FileStorage', () => {
   let accessKeyId: string
   let secretAccessKey: string
+  let bucket: string
+  let key: string
+  let file: Buffer
   let sut: AwsS3FileStorage
+  let putObjectPromiseSpy: jest.Mock
+  let putObjectSpy: jest.Mock
 
   beforeAll(() => {
     accessKeyId = 'any_access_key_id'
     secretAccessKey = 'any_secret_access_key'
+    bucket = 'any_bucket'
+    key = 'any_key'
+    file = Buffer.from('any_buffer')
+    putObjectPromiseSpy = jest.fn()
+    putObjectSpy = jest.fn().mockImplementation(() => ({ promise: putObjectPromiseSpy }))
+    mocked(S3).mockImplementation(jest.fn().mockImplementation(() => ({ putObject: putObjectSpy })))
   })
 
   beforeEach(() => {
-    sut = new AwsS3FileStorage(accessKeyId, secretAccessKey)
+    sut = new AwsS3FileStorage(accessKeyId, secretAccessKey, bucket)
   })
 
   it('should config aws credentials on create', () => {
-    expect(sut).toBeDefined()
     expect(config.update).toHaveBeenCalledWith({ credentials: { accessKeyId, secretAccessKey } })
     expect(config.update).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call putObject with correct input', async () => {
+    await sut.upload({ file, key })
+
+    expect(putObjectSpy).toBeCalledWith(
+      {
+        Bucket: bucket,
+        Key: key,
+        Body: file,
+        ACL: 'public-read'
+      }
+    )
+    expect(putObjectSpy).toHaveBeenCalledTimes(1)
+    expect(putObjectPromiseSpy).toHaveBeenCalledTimes(1)
   })
 })
 
 class AwsS3FileStorage {
   constructor (
-    private readonly accessKeyId: string,
-    private readonly secretAccessKey: string
+    accessKeyId: string,
+    secretAccessKey: string,
+    private readonly bucket: String
   ) {
     config.update({ credentials: { accessKeyId, secretAccessKey } })
+  }
+
+  async upload ({ file, key }: UploadFile.Input): Promise<void> {
+    const s3 = new S3()
+    await s3.putObject({
+      Bucket: this.bucket,
+      Key: key,
+      Body: file,
+      ACL: 'public-read'
+    }).promise()
   }
 }
